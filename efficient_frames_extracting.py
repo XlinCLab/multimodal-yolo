83ea6995e647c6f29603c1734e0868bd608dd841
Philip Georgis <philip.georgis@gmail.com>
2026-01-21 12:25:00 +0100

    Correct path to output frame file

--- efficient_frames_extracting.py
+++ efficient_frames_extracting.py
@@ -32,7 +32,7 @@ def extract_frames(video_path: str, video_frames: pd.DataFrame, outdir: str = ""
         if not ret:
             break  # Break if the frame can't be read
         #the frames will be saved in the folder 'frames_to_recognize' in the root folder
-        frame_filename = os.path.join(outdir, f'/data/images/set{participant}_session{session}_frame_{frame_number}.jpg')
+        frame_filename = os.path.join(outdir, f'data/images/set{participant}_session{session}_frame_{frame_number}.jpg')
         frame_dirname = os.path.dirname(frame_filename)
         os.makedirs(frame_dirname, exist_ok=True)
         cv2.imwrite(frame_filename, frame)
